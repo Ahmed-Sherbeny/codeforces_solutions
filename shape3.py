10860d0e2cfb77852225b4e7c8c442657dc93b94
ahmed <ahmedsherbeny2004@gmail.com>
2025-06-20 20:35:52 +0300

    shape3 sol

---
+++ shape3.py
@@ -0,0 +1,10 @@
+N = int(input())
+for i in range(1, N + 1):
+    spaces = ' ' * (N - i)
+    stars = '*' * (2 * i - 1)
+    print(spaces + stars)
+
+for i in range(N, 0, -1):
+    spaces = ' ' * (N - i)
+    stars = '*' * (2 * i - 1)
+    print(spaces + stars)
