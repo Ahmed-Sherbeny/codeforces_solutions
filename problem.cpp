8a2bac827534cdf5176765426e90e59bf34c8979
ahmed <ahmedsherbeny2004@gmail.com>
2025-06-20 16:18:37 +0300

    first commit

---
+++ problem.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <string.h>
+#include <cctype>
+#include <map> 
+using namespace std;
+
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int s, n, x, y;
+    cin >> s >> n;
+    
+    while (n--)
+    {
+        cin >> x >> y;
+       solve(s, n, x, y);
+        // cout << solution;
+    }
+
+    return 0;
+}
+
