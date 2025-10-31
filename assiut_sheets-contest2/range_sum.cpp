63181fc79670ad925122a8f9fc2ba217782fd440
ahmed_sherbeny <ahmed.elsherbeny@ejust.edu.eg>
2025-10-31 18:29:07 +0200

    range sum sol

---
+++ assiut_sheets-contest2/range_sum.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <cctype>
+#include <map>
+#define ll long long
+using namespace std;
+
+
+ll rangeSum(ll L, ll R) {
+    ll sumR = R * (R + 1) / 2;
+    ll sumL = (L - 1) * L / 2;
+    return sumR - sumL;
+}
+
+int main(void)
+{
+      ios_base::sync_with_stdio(false);
+      cin.tie(NULL);
+
+      int t;
+      cin >> t;
+      // int X;
+      // cin >> X;
+      ll l, r;
+      
+
+      while(t--) {
+        cin >> l >> r;
+        cout << rangeSum(l, r) << '\n';
+    }
+
+      return 0;
+}
